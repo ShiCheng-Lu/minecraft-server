3b2f0626f1c699ed0ba994f81952c0c295f3dabe
ShiCheng Lu <shichenglu0708@gmail.com>
2026-02-02 21:50:28 -0500

    download from url

--- download_paper.sh
+++ download_paper.sh
@@ -3,16 +3,16 @@
 # https://docs.papermc.io/misc/downloads-service/
 
 PROJECT="paper"
-MINECRAFT_VERSION="1.21.11"
 USER_AGENT="uw-archery-minecraft/1.0.0 (sc3lu@uwaterloo.com)"
-
 
 LATEST_VERSION=$(curl -s -H "User-Agent: $USER_AGENT" https://fill.papermc.io/v3/projects/${PROJECT} | \
     jq -r '.versions | to_entries[0] | .value[0]')
 
 echo "Latest version is $LATEST_VERSION"
 
-PAPERMC_URL=$(curl -s -H "User-Agent: $USER_AGENT" https://fill.papermc.io/v3/projects/${PROJECT}/versions/${MINECRAFT_VERSION}/builds \
+PAPERMC_URL=$(curl -s -H "User-Agent: $USER_AGENT" https://fill.papermc.io/v3/projects/${PROJECT}/versions/${LATEST_VERSION}/builds \
     | jq -r 'first(.[] | select(.channel == "STABLE") | .downloads."server:default".url) // "null"')
 
-echo $PAPERMC_URL
+echo "Latest build url is $PAPERMC_URL"
+
+curl -o server.jar $PAPERMC_URL
