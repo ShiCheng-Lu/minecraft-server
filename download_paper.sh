1b9136fb2bdecd5138a2e79f8128358fb503bc08
ShiCheng Lu <shichenglu0708@gmail.com>
2026-02-02 21:44:38 -0500

    get url directly

--- download_paper.sh
+++ download_paper.sh
@@ -6,16 +6,7 @@ PROJECT="paper"
 MINECRAFT_VERSION="1.21.11"
 USER_AGENT="uw-archery-minecraft/1.0.0 (sc3lu@uwaterloo.com)"
 
-BUILDS_RESPONSE=$(curl -s -H "User-Agent: $USER_AGENT" https://fill.papermc.io/v3/projects/${PROJECT}/versions/${MINECRAFT_VERSION}/builds)
-
-# Check if the API returned an error
-if echo "$BUILDS_RESPONSE" | jq -e '.ok == false' > /dev/null 2>&1; then
-  ERROR_MSG=$(echo "$BUILDS_RESPONSE" | jq -r '.message // "Unknown error"')
-  echo "Error: $ERROR_MSG"
-  exit 1
-fi
-
-# Try to get a stable build URL for the requested version
-PAPERMC_URL=$(echo "$BUILDS_RESPONSE" | jq -r 'first(.[] | select(.channel == "STABLE") | .downloads."server:default".url) // "null"')
+PAPERMC_URL=$(curl -s -H "User-Agent: $USER_AGENT" https://fill.papermc.io/v3/projects/${PROJECT}/versions/${MINECRAFT_VERSION}/builds) \
+    | jq -r 'first(.[] | select(.channel == "STABLE") | .downloads."server:default".url) // "null"')
 
 echo $PAPERMC_URL
