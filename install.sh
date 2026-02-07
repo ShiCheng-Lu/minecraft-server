e6ff15e06b56eb50438965e3b4c52684acbc0c35
ShiCheng Lu <shichenglu0708@gmail.com>
2026-02-06 20:58:44 -0500

    add readme

--- install.sh
+++ install.sh
@@ -2,13 +2,7 @@
 
 
 echo "=== Installing latest java ==="
-sudo apt-get update
-sudo apt-get install ca-certificates apt-transport-https gnupg wget
-wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
-echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
-sudo apt-get update
-sudo apt-get install -y java-21-amazon-corretto-jdk libxi6 libxtst6 libxrender1
-
-java -version
+sh install_java.sh
 
 echo "=== Getting the server jar ==="
+sh download_paper.sh
